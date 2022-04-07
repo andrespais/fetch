ef60bdf577a5f9a14b985deaee49b49ba5d0f18b
Andrés País <andrespais61@gmail.com>
2022-04-06 23:58:23 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,293 @@
+const agregarAlCarrito = (producto) => {
+    carrito.push(producto);
+};
+
+const carrito = [];
+
+const productos = [
+    { id: 1, titulo: "Darwin", precio: 999, stock:0, imagen: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/4B38/production/_107365291_gettyimages-526191956.jpg' },
+    { id: 2, titulo: "Turing", precio: 222, stock:32, imagen: 'https://www.biografiasyvidas.com/biografia/t/fotos/turing.jpg' },
+    { id: 3, titulo: "Newton", precio: 111, stock:0, imagen: 'https://dotorqantico.files.wordpress.com/2013/08/newton.jpg'},
+    { id: 4, titulo: "Einstein", precio: 1982, stock: 56, imagen: 'https://ichef.bbci.co.uk/news/640/amz/worldservice/live/assets/images/2015/04/17/150417194023_einstein-lengua.jpg' },
+];
+
+generarCards(productos);
+
+function generarCards(productosAMostrar){
+    let acumuladorDeCards = ``;
+    productosAMostrar.forEach((elementoDelArray) => {
+        acumuladorDeCards += `<div class="col mb-5">
+        <div class="card h-100">
+            <!-- Sale badge-->
+            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">
+                ${(elementoDelArray.stock > 0) ? 'Esta en venta' : 'Sin stock'}
+            </div>
+            <!-- Product image-->
+            <img class="card-img-top" src="${elementoDelArray.imagen}" alt="..." />
+            <!-- Product details-->
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <!-- Product name-->
+                    <h5 class="fw-bolder">${elementoDelArray.titulo}</h5>
+                    <!-- Product price-->
+                    <span class="text-muted text-decoration-line-through">$20.00</span>
+                    $${elementoDelArray.precio}
+                </div>
+            </div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Agregar al carrito</a></div>
+            </div>
+        </div>
+    </div>`;
+    });
+    mostrarCardsEnElHTML(acumuladorDeCards);
+}
+
+function mostrarCardsEnElHTML(cards) {
+    document.getElementById("listado-productos").innerHTML = cards;
+};
+
+
+function buscarProducto() {
+    const nombreProductoBuscado = document.getElementById("producto-buscado").value.toUpperCase().trim();
+
+    const productosEncontrados = productos.filter((producto) => {
+        return producto.titulo.toUpperCase().match(nombreProductoBuscado);
+    });
+
+    generarCards(productosEncontrados);
+}
+
+var timeOut;
+
+class Item {
+    constructor(icon, backgroundColor) {
+        this.$element = $(document.createElement("div"));
+        this.icon = icon;
+        this.$element.addClass("item");
+        this.$element.css("background-color", backgroundColor);
+        var i = document.createElement("i");
+        $(i).addClass("fi-" + icon);
+        this.$element.append(i);
+        this.prev = null;
+        this.next = null;
+        this.isMoving = false;
+        var element = this;
+        this.$element.on("mousemove", function() {
+            clearTimeout(timeOut);
+            timeOut = setTimeout(function() {
+                if (element.next && element.isMoving) {
+                    element.next.moveTo(element);
+                } 
+            }, 10);
+        });
+    }
+    
+    moveTo(item) {
+        anime({
+            targets: this.$element[0],
+            left: item.$element.css("left"),
+            top: item.$element.css("top"),
+            duration: 700,
+            elasticity: 500
+        });
+        if (this.next) {
+            this.next.moveTo(item);
+        }
+    }
+
+    updatePosition() {    
+        anime({
+            targets: this.$element[0],
+            left: this.prev.$element.css("left"),
+            top: this.prev.$element.css("top"),
+            duration: 80
+        });
+        
+        if (this.next) {
+            this.next.updatePosition();
+        }
+    }
+}
+
+class Menu {
+    constructor(menu) {
+        this.$element = $(menu);
+        this.size = 0;
+        this.first = null;
+        this.last = null;
+        this.timeOut = null;
+        this.hasMoved = false;
+        this.status = "closed";
+    }
+    
+    add(item) {
+        var menu = this;
+        if (this.first == null) {
+            this.first = item;
+            this.last = item;
+            this.first.$element.on("mouseup", function() {
+                if (menu.first.isMoving) {
+                    menu.first.isMoving = false;        
+                } else {
+                    menu.click();
+                }
+            }); 
+            item.$element.draggable(
+                {
+                    start: function() {
+                        menu.close();
+                        item.isMoving = true;
+                    }  
+                },
+                {
+                    drag: function() {
+                        if (item.next) {
+                            item.next.updatePosition();
+                        }
+                    }
+                },
+                {
+                    stop: function() {
+                        item.isMoving = false;
+                        item.next.moveTo(item);
+                    }
+                }
+            );
+        } else {
+            this.last.next = item;
+            item.prev = this.last;
+            this.last = item;
+        }
+        this.$element.after(item.$element);
+        
+        
+    }
+    
+    open() {
+        this.status = "open";
+        var current = this.first.next;
+        var iterator = 1;
+        var head = this.first;
+        var sens = head.$element.css("left") < head.$element.css("right") ? 1 : -1;
+        while (current != null) {
+            anime({
+                targets: current.$element[0],
+                left: parseInt(head.$element.css("left"), 10) + (sens * (iterator * 50)),
+                top: head.$element.css("top"),
+                duration: 500
+            });
+            iterator++;
+            current = current.next;
+        }    
+    }
+    
+    close() {
+        this.status = "closed";
+        var current = this.first.next;
+        var head = this.first;
+        var iterator = 1;
+        while (current != null) {
+            anime({
+                targets: current.$element[0],
+                left: head.$element.css("left"),
+                top: head.$element.css("top"),
+                duration: 500
+            });
+            iterator++;
+            current = current.next;
+        }
+    }
+    
+    click() {
+        if (this.status == "closed") {
+            this.open();
+        } else {
+            this.close();
+        }
+    }
+    
+}
+
+var menu = new Menu("#myMenu");
+var item1 = new Item("list");
+var item2 = new Item("torso", "#FF5C5C");
+var item3 = new Item("social-facebook", "#5CD1FF");
+var item4 = new Item("paypal", "#FFF15C");
+var item5 = new Item("link", "#64F592");
+
+menu.add(item1);
+menu.add(item2);
+menu.add(item3);
+menu.add(item4);
+menu.add(item5);
+$(document).delay(50).queue(function(next) {
+    menu.open();
+    next();
+    $(document).delay(1000).queue(function(next) {
+        menu.close();
+        next();
+    });
+});
+
+var basicTimeline = anime.timeline({
+    autoplay: false
+  });
+  
+  var pathEls = $(".check");
+  for (var i = 0; i < pathEls.length; i++) {
+    var pathEl = pathEls[i];
+    var offset = anime.setDashoffset(pathEl);
+    pathEl.setAttribute("stroke-dashoffset", offset);
+  }
+  
+  basicTimeline
+    .add({
+      targets: ".text",
+      duration: 1,
+      opacity: "0"
+    })
+    .add({
+      targets: ".button",
+      duration: 1300,
+      height: 10,
+      width: 300,
+      backgroundColor: "#2B2D2F",
+      border: "0",
+      borderRadius: 100
+    })
+    .add({
+      targets: ".progress-bar",
+      duration: 2000,
+      width: 300,
+      easing: "linear"
+    })
+    .add({
+      targets: ".button",
+      width: 0,
+      duration: 1
+    })
+    .add({
+      targets: ".progress-bar",
+      width: 80,
+      height: 80,
+      delay: 500,
+      duration: 750,
+      borderRadius: 80,
+      backgroundColor: "#71DFBE"
+    })
+    .add({
+      targets: pathEl,
+      strokeDashoffset: [offset, 0],
+      duration: 200,
+      easing: "easeInOutSine"
+    });
+  
+  $(".button").click(function() {
+    basicTimeline.play();
+  });
+  
+  $(".text").click(function() {
+    basicTimeline.play();
+  });
